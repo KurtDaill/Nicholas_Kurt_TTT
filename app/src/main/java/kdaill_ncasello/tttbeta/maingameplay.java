4a80bc4b43868d6ad28c0d618512b932e173aaee
nicholas <ncarsello@luc.edu>
2018-09-22 17:25:28 -0500

    Starting the UI/allowing for user input

---
+++ app/src/main/java/kdaill_ncasello/tttbeta/maingameplay.java
@@ -0,0 +1,24 @@
+package kdaill_ncasello.tttbeta
+
+import android.support.v7.app.AppCompatActivity;
+import java.util.Scanner;
+import java.lang.Object;
+import android.os.Bundle;
+
+
+public class maingameplay : AppCompatActivity(){
+
+        gameengine UserInput = new gameengine();
+        }
+
+public static interface View.OnClickListener {
+
+        }
+
+
+public class Object{
+    java.lang.Object;
+}
+
+
+        }
